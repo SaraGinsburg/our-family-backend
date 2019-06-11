0f99f6a967343b06f2964eac5e953c6354d88551
Sara Ginsburg <Sara.ginsburg.ny@gmail.com>
2019-06-11 15:05:24 -0400

    allow origins to be 3000 or 3001

--- config/routes.rb
+++ config/routes.rb
@@ -3,23 +3,6 @@ Rails.application.routes.draw do
     namespace :v1   do
 
       resources :siblings
-
-      resources :parents do
-        resources :siblings, :path => "children"
-      end
-
-      resources :siblings do
-        resources :milestones
-      end
-
-      resources :siblings do
-        resources :kind_words
-      end
-
-      resources :siblings do
-        resources  :nice_deeds
-      end
-
       resources :milestones
       resources :kind_words
       resources :nice_deeds
@@ -29,3 +12,18 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+# resources :parents do
+#   resources :siblings, :path => "children"
+# end
+#
+# resources :siblings do
+#   resources :milestones
+# end
+#
+# resources :siblings do
+#   resources :kind_words
+# end
+#
+# resources :siblings do
+#   resources  :nice_deeds
+# end
