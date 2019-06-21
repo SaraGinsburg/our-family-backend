4e3955ef1648a1d1c077b99d4553e6c64b2ed03f
Sara Ginsburg <Sara.ginsburg.ny@gmail.com>
2019-06-21 01:55:39 -0400

    add sessions#destroy for logout

--- app/controllers/API/V1/sessions_controller.rb
+++ app/controllers/API/V1/sessions_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::SessionsController < ApplicationController
 
   def create
     @user = User.find_by(username: params[:session][:username])
-
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
       render json:UserSerializer.new(@user)
